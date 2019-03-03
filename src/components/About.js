0aa7161a2d0da172d7626612145cd925d066a258
Lana Monson <lanamonson.slc@gmail.com>
2019-03-03 13:16:19 -0700

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './About.css';
+
+class About extends Component {
+  render() {
+    return (
+      <div className="About">
+        <h1>ABOUT ME</h1>
+        
+      </div>
+    );
+  }
+}
+
+export default About;
