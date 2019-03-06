6a57413a2055a553f6b7ea10e7f22c6110440a2b
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-03-05 20:13:01 -0700

    Delete index.js

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
-import Routing from './Routing';
-
-ReactDOM.render(<Routing />, document.getElementById('root'));
-
-serviceWorker.unregister();
